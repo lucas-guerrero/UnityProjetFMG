adae141dcaee47703ad942fa3da2e22035cb383b
LiMaO90 <lucasguerrero741@gmail.com>
2021-11-22 21:49:00 +0100

    Ajout collision + ajustement Camera vertical + ajout fenetre

--- Assets/Script/Personnage/DeplacementPersonnage.cs
+++ Assets/Script/Personnage/DeplacementPersonnage.cs
@@ -4,36 +4,71 @@ using UnityEngine;
 
 public class DeplacementPersonnage : MonoBehaviour
 {
+    [SerializeField] public Camera cameraPlayer;
+
     [SerializeField] private float speedMove;
-    [SerializeField] private float speedCamera;
-    private Transform transformObject;
+    [SerializeField] private float speedCamera = 200f;
+    [SerializeField] private float maxViewVertical = 45f;
+    [SerializeField] private float minViewVertical = -30f;
+
+    private Transform transformPlayer;
+    private float viewCameraVertical = 0f;
+    private bool isCollision = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        transformObject = gameObject.GetComponent<Transform>();
+        transformPlayer = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKey("up")) {
-            transformObject.Translate(0, 0, speedMove*Time.deltaTime);
+            transformPlayer.Translate(0, 0, speedMove*Time.deltaTime);
         }
+
         if(Input.GetKey("down")) {
-            transformObject.Translate(0, 0, -speedMove * Time.deltaTime);
+            transformPlayer.Translate(0, 0, -speedMove * Time.deltaTime);
         }
+
         if(Input.GetKey("left")) {
-            transformObject.Translate(-speedMove * Time.deltaTime, 0, 0);
+            transformPlayer.Translate(-speedMove * Time.deltaTime, 0, 0);
         }
+
         if(Input.GetKey("right")) {
-            transformObject.Translate(speedMove * Time.deltaTime, 0, 0);
+            transformPlayer.Translate(speedMove * Time.deltaTime, 0, 0);
         }
-        if(Input.GetAxis("Mouse X")<0){
-            transformObject.Rotate(0, -speedCamera * Time.deltaTime, 0);
+
+        if(Input.GetAxis("Mouse X") != 0f ){
+
+
+            transformPlayer.Rotate(0, Input.GetAxisRaw("Mouse X") * 0.01f * speedCamera, 0);
         }
-        if(Input.GetAxis("Mouse X")>0){
-            transformObject.Rotate(0, speedCamera * Time.deltaTime, 0);
+
+        if(Input.GetAxis("Mouse Y") != 0f){
+
+            if(viewCameraVertical <= maxViewVertical && viewCameraVertical >= minViewVertical) {
+                float tmp = Input.GetAxisRaw("Mouse Y");
+                tmp *= -0.01f;
+
+                viewCameraVertical += tmp * speedCamera;
+                viewCameraVertical = Mathf.Clamp(viewCameraVertical, -89f, 89f);
+
+                cameraPlayer.transform.localEulerAngles = new Vector3(viewCameraVertical, 0, 0);
+            }
+            else {
+                if(viewCameraVertical > maxViewVertical) viewCameraVertical = maxViewVertical;
+                else viewCameraVertical = minViewVertical;
+            }
         }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        isCollision = true;
+    }
+
+    void OnCollisionExit(Collision collision) {
+        isCollision = false;
     }
 }
